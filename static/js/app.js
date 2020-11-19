b050cab06432f5cd72bb1ec86e6b432a12bc7427
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-11-19 05:13:30 -0600

    adding in demographic table return results

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,19 @@
 // If using python server, visit here to see data: http://localhost:8000/data/data.json
-var metaData=[];
+var metaData = [];
 
 console.log('test');
 d3.json("data/samples.json").then((data) => {
     console.log(data);
 
-    var selector=d3.select('#selDataset')
+    var selector = d3.select('#selDataset')
     var names = data.names;
-/*     for (var i =0; i<names.length; i++){
-        selector.append('option').text(names[i])
-    }; */
-    names.forEach(sample=>{
+    /*     for (var i =0; i<names.length; i++){
+            selector.append('option').text(names[i])
+        }; */
+    names.forEach(sample => {
         selector.append('option')
-        .text(sample)
-        .property('value', sample)
+            .text(sample)
+            .property('value', sample)
     })
 
 
@@ -23,16 +23,18 @@ d3.json("data/samples.json").then((data) => {
     console.log(names);
     console.log(metaData);
     console.log(samples);
-    console.log(id);
-
 });
 
-function optionChanged(selectedID){
+function optionChanged(selectedID) {
     console.log(selectedID);
-   var metaDataSelector = metaData.filter(oneMeta=>oneMeta.id === parseInt(selectedID))[0];
-   console.log(metaDataSelector)
-   Object.Entries(metaDataSelector)
-    
+    var metaDataSelector = metaData.filter(oneMeta => oneMeta.id === parseInt(selectedID))[0];
+    console.log(metaDataSelector);
+    Object.entries(metaDataSelector).forEach(([key, value]) => {
+        console.log(key, value);
+        var panel = d3.select("#panel-info");
+        panel.append('tr')
+            .text(`${key}: ${value}`);
+    });
 };
 
 /* function callbackFun(){
