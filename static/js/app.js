c8923e72983dbb75890e045a7f1d2e47781d1a7b
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-11-21 19:55:14 -0600

    making headway to on the charts

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,9 @@ var samples = [];
 var sampleId = [];
 var sampleValues = [];
 
-console.log('test');
+// console.log('test');
 d3.json("data/samples.json").then((data) => {
-    console.log(data);
+    // console.log(data);
 
     var selector = d3.select('#selDataset')
     var names = data.names;
@@ -20,60 +20,64 @@ d3.json("data/samples.json").then((data) => {
     samples = data.samples;
     metaData = data.metadata;
     var id = metaData.id;
-    console.log(names);
-    console.log(metaData);
-    console.log(samples);
-    console.log(id);
+    // console.log(names);
+    // console.log(metaData);
+    // console.log(samples);
 });
 
 
 
 function optionChanged(selectedID) {
-    console.log(selectedID);
+    // console.log(selectedID);
     // Declare MetaData Object variable and filter to matching id array from option
     var metaDataSelector = metaData.filter(oneMeta => oneMeta.id === parseInt(selectedID))[0];
-    console.log(metaDataSelector);
+    // console.log(metaDataSelector);
     //Declare Sample Object variable and filter to matching id array from option
-    var sampleSelector = samples.filter(sample => sample.id === (selectedID))[0];
-    console.log(sampleSelector);
+    var sampleSelector = samples.filter(sample => sample.id === (selectedID));
+    // console.log(sampleSelector);
+
+
     //select html panel element 
     var panel = d3.select("#panel-info")
     // remove any data from the table
     panel.html("");
 
     // Declare list variable for appending array data to panel element
-    var list = panel.append('ul')
+    var list = panel.append('ul').attr('class', 'list-group list-group-flush')
     Object.entries(metaDataSelector).forEach(([key, value]) => {
-        console.log(key, value);
-        list.append('li')
-            .text(`${key}: ${value}`);
-        console.log(key, value);
+        list.append('li').attr('class', 'list-group-item')
+            .html(`<strong>${key}</strong>: ${value}`);
+        // console.log(key, value);
     });
 
     //iterate through sample data to return matching sample id and value data for bar chart
 
     // Sort the data
-    var sortSample = sampleSelector.sample_values.sort((a, b) => b.sample_values - a.sample_values);
+    var sortSample = sampleSelector.sort((a, b) => b.sample_values - a.sample_values);
+    // console.log(sortSample);
     // Slice the first 10 objects for plotting
-    sliceSample = sortSample.slice(0, 10);
-    // Reverse the array to accommodate Plotly's defaults
-    reversedData = sliceSample.reverse();
-    console.log(reversedData);
-    
+    SampleValues = sortSample[0].sample_values;
+    SampleIds = sortSample[0].otu_ids;
+    SampleLabels = sortSample[0].otu_labels;
+    console.log(sortSample);
 
-    
+    sliceValues = SampleValues.slice(0, 10).reverse();
+    sliceIds = SampleIds.slice(0, 10).reverse();
+    sliceLabels = SampleLabels.slice(0, 10).reverse();
+    console.log(sliceSampleValues);
 
-    // Trace1 for the Greek Data
+
+    // Trace1 for the Belly Button Data
     var trace1 = {
-        x: reversedData.sample_values,
-        y: reversedData.otu_labels,
-        text: reversedData.otu_labels,
-        name: "Sample Data",
+        x: sliceValues,
+        y: sliceIds.map(d => `OTU Id: ${d}`),
+        text: sliceLabels,
+        name: "Belly Button Data",
         type: "bar",
         orientation: "h"
     };
     // data
-    var data = [trace1];
+    var sliceSampleValues = [trace1];
 
     // Apply the group bar mode to the layout
     var layout = {
@@ -85,7 +89,27 @@ function optionChanged(selectedID) {
             b: 100
         }
     };
+// Render the plot to the div tag with id "bubble"
+    Plotly.newPlot("bar", sliceSampleValues, layout);
 
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", data, layout);
+    var trace1 = {
+        x: SampleValues,
+        y: SampleIds,
+        mode: 'markers',
+        marker: {
+            size: SampleValues
+        }
+    };
+
+    var SampleValues = [trace1];
+
+    var layout = {
+        title: 'Marker Size',
+        showlegend: true,
+        height: 600,
+        width: 600
+    };
+
+    // Render the plot to the div tag with id "bubble"
+     Plotly.newPlot('bubble', SampleValues, layout);
 };
